9239438f4e220a298def58fc36f0a0cc5c8e9a20
Aki <ancgrouptx@gmail.com>
2023-07-10 09:36:33 -0500

    html Line 161,162 are edited

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ playBoard = document.querySelector(".play-board"),
 players = document.querySelector(".players"),
 allBox = document.querySelectorAll("section span"),
 resultBox = document.querySelector(".result-box"),
-wonText = resultBox.querySelector(".won-text"),
+wonText = resultBox.querySelector(".you-won-text"),
 replayBtn = resultBox.querySelector("button");
 
 window.onload = ()=>{
